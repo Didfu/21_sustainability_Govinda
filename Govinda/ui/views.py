f6825ae0ee1d1367bf8b9766008d388dd0ec8f39
dhruvmahyavanshi <mahyavanshidhruv@gmail.com>
2024-04-07 01:08:59 +0530

    designing

--- Govinda/ui/views.py
+++ Govinda/ui/views.py
@@ -9,6 +9,14 @@ def month_view(request):
     return render(request, 'web/month.html')
 
 def input_view(request):
+    if request.method == 'POST':
+        date = request.POST.get('date')
+        time = request.POST.get('time')
+        air_temp = request.POST.get('airTemperature')
+        wind_speed = request.POST.get('windSpeed')
+        print(date,time, air_temp, wind_speed)
+    # Process the input data
+    # ...
     return render(request, 'web/input.html')
 
 def govinda_view(request):
